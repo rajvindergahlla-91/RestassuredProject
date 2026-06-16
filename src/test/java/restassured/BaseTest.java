9f3a2f7b9eef63c5f47fa4e8af3211a213672d26
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-06-16 19:44:45 -0400

    api testing data

---
+++ src/test/java/restassured/BaseTest.java
@@ -0,0 +1,40 @@
+package restassured;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.testng.annotations.BeforeSuite;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class BaseTest {
+	
+	protected static String token;
+
+    @BeforeSuite
+    	public void generateToken() throws Exception {
+
+    	    String requestBody = new String(
+    	            Files.readAllBytes(
+    	                    Paths.get("src/test/resources/payloads/loginUser.json")));
+
+    	    RestAssured.baseURI =
+    	            "https://report-ease-prj-666-naa.vercel.app";
+
+    	    Response response =
+    	            RestAssured.given()
+    	                       .header("Content-Type", "application/json")
+    	                       .body(requestBody)
+    	                    .when()
+    	                       .post("api/login");
+
+    	    response.then().statusCode(200);
+
+    	    token = response.jsonPath().getString("token");
+
+    	    System.out.println("Generated Token : " + token);
+    	}
+    }
+
+
--- src/test/java/restassured/RestAssuredAptTest.java
+++ src/test/java/restassured/RestAssuredAptTest.java
@@ -3,25 +3,38 @@ package restassured;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 
-public class RestAssuredAptTest {
-	@Test
-	
-	public void postRequest()
-	{
-		String requestBody= "{\r\n"
-				+ "\"name\": \"John Doe\",\r\n"
-				+ "\"email\": \"john@example.com\", \"password\": \"SecurePass@123\", \"role\": \"resident\"\r\n"
-				+ "}\r\n"
-				+ "";
-		RestAssured.baseURI="https://report-ease-prj-666-naa.vercel.app";
-		RestAssured.given()
-		               .header("Content-Type", "application/json" )
-		               .body(requestBody)
-		             .when()
-		               .post("api/register")
-		             .then()
-		               .statusCode(201);
-	} 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import io.restassured.response.Response;
 
-	
+public class RestAssuredAptTest extends BaseTest {
+	@Test
+
+	public void postRegisterRequest() throws Exception {
+
+		String requestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/payloads/registerUser.json")));
+
+		RestAssured.baseURI = "https://report-ease-prj-666-naa.vercel.app";
+		RestAssured.given().header("Content-Type", "application/json").body(requestBody).when().post("api/register")
+				.then().statusCode(201);
+	}
+
+	@Test
+
+	public void postLogInRequest() throws Exception {
+		String requestBody = new String(Files.readAllBytes(Paths.get("src/test/resources/payloads/loginUser.json")));
+
+		RestAssured.baseURI = "https://report-ease-prj-666-naa.vercel.app";
+		Response response = RestAssured.given().header("Content-Type", "application/json").body(requestBody).when()
+				.post("api/login");
+
+		response.then().statusCode(200);
+		response.prettyPrint();
+
+		String token = response.jsonPath().getString("token");
+
+		System.out.println(token);
+
+	}
+
 }
